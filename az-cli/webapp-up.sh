fb55e4b55b43baca299e1c0a513657305646dcac
AJ <90054101+Trainer-AJ@users.noreply.github.com>
2024-04-21 16:54:48 +0530

    Update webapp-up.sh

--- az-cli/webapp-up.sh
+++ az-cli/webapp-up.sh
@@ -1,3 +1,3 @@
 az group create -n leo -l westus
 # be in the directory where you have the app files
-az webapp up -g "leo" -l eastus --html -n demo-0989 --sku F1
+az webapp up -g "leo" -l centralindia --html -n demo-098967 --sku F1
